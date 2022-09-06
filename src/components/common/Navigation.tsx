8198d4afcc704a49bbb4ec7df9ee8ddd2f932bae
kitez <duswl339@ajou.ac.kr>
2022-09-07 01:11:29 +0900

    feat: component TopBar / TopBar 내부 dropdown menu의 경우 사용 시 약간의 커스터마이징이 필요함

--- src/components/common/Navigation.tsx
+++ src/components/common/Navigation.tsx
@@ -1,18 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, { Dispatch, SetStateAction, useCallback, useEffect, useState } from "react";
 import '../../styles/components/common/Navigation.css';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 import {
-  // eslint-disable-next-line max-len
   RiInboxArchiveLine,
   RiUser3Line,
   RiVideoAddLine,
   RiNotification2Line,
   RiHeart3Line,
   RiQuestionAnswerLine,
+  RiShareForwardLine,
   RiShareForwardFill,
   RiListUnordered,
-  RiHeart3Fill,
-} from 'react-icons/ri';
+  RiHeart3Fill, RiArrowLeftLine, RiCheckFill, RiMore2Fill
+} from "react-icons/ri";
+import { ITopBar, IVerticalNavigation } from "../../globalType";
 
 const defaultCurrent = {
   home: true,
@@ -22,17 +24,8 @@ const defaultCurrent = {
 };
 
 const BottomNavigation = function () {
-  const [color, setColor] = useState('white');
 
-  useEffect(()=>{
-    console.log("Vertical : ",color);
-
-    if(window.location.pathname === '/home'){
-      setColor('white');
-    }else{
-      setColor('black');
-    }
-  },[window.location.pathname]);
+  const color ='white';
 
   return (
     <div className="bottom-nav-container">
@@ -48,7 +41,13 @@ const TopNavigation = function () {
   const navigation = useNavigate();
 
   useEffect(() => {
-    console.log(current);
+    if (current.home) {
+      navigation('/home');
+    } else if (current.search) {
+      navigation('/search');
+    } else {
+      navigation('/following');
+    }
   }, [current]);
 
   const [topNavContainerStyle, setTopNavContainerStyle] = useState('top-nav-text-container style-home');
@@ -56,11 +55,9 @@ const TopNavigation = function () {
   const onClickHandlerTopNavigation = function (e: React.MouseEvent) {
     e.preventDefault();
 
-    console.log(e.currentTarget.innerHTML);
     if (e.currentTarget.innerHTML === '홈') {
       setCurrent(defaultCurrent);
       setTopNavContainerStyle('top-nav-text-container style-home');
-      navigation('/home');
     } else if (e.currentTarget.innerHTML === '검색') {
       setCurrent({
         ...defaultCurrent,
@@ -69,7 +66,6 @@ const TopNavigation = function () {
         alarmColor: 'black',
       });
       setTopNavContainerStyle('top-nav-text-container style-not-home');
-      navigation('/search');
     } else {
       setCurrent({
         ...defaultCurrent,
@@ -78,7 +74,6 @@ const TopNavigation = function () {
         alarmColor: 'black',
       });
       setTopNavContainerStyle('top-nav-text-container style-not-home');
-      navigation('/following');
     }
   };
 
@@ -109,53 +104,163 @@ const TopNavigation = function () {
   );
 };
 
-const VerticalNavigation = function () {
+const VerticalNavigation = function ({isSelectedInfo,setIsSelectedInfo,isSelectedHeart,setIsSelectedHeart,isSelectComment , setIsSelectComment, isClickedShare, setIsClickedShare} : IVerticalNavigation ) {
   // Props
   // 해당 숏폼을 좋아요 눌렀는지
   // 해당 숏폼에 대한 정보
   // 해당 숏폼에 달린 댓글 정보
 
-  const [like, setLike] = useState(false);
-  const [color, setColor] = useState('white');
 
-  useEffect(()=>{
-    if (window.location.pathname === '/home') {
-      setColor('white');
-    } else {
-      setColor('black');
-    }
+  const onClickShare = useCallback(()=>{
+    setIsClickedShare(!isClickedShare);
+  },[isClickedShare]);
 
-    console.log("Bottom");
-    console.log(window.location.pathname);
-    console.log(color);
-  },[window.location.pathname]);
+  const onClickHeart = useCallback(()=>{
+    setIsSelectedHeart(!isSelectedHeart);
+  },[isSelectedHeart]);
 
+  const onClickComment = useCallback(()=>{
+    setIsSelectComment(true);
+  },[isSelectComment]);
 
+  const onClickInfo = useCallback(()=>{
+    setIsSelectedInfo(!isSelectedInfo);
+  },[isSelectedInfo]);
 
-  return (
-    <div className="vertical-navigation-container">
-      {like ? (
-        <RiHeart3Fill
-          fill={color}
-          onClick={() => {
-            setLike(false);
-          }}
-          className="icon"
-        />
-      ) : (
-        <RiHeart3Line
-          fill={color}
-          onClick={() => {
-            setLike(true);
-          }}
-          className="icon"
-        />
-      )}
-      <RiQuestionAnswerLine fill={color} className="icon" />
-      <RiShareForwardFill fill={color} className="icon" />
-      <RiListUnordered fill={color} className="icon" />
-    </div>
+  return(
+    <section className={"vertical-navigation-container"}>
+      { isSelectedHeart ? <RiHeart3Fill onClick={onClickHeart} className={"icon"}/> : <RiHeart3Line onClick={onClickHeart} className={"icon"} /> }
+      <RiQuestionAnswerLine onClick={onClickComment} className={"icon"} />
+      <RiShareForwardLine onClick={onClickShare} className={"icon"} />
+      <RiListUnordered onClick={onClickInfo} className={"icon"} />
+    </section>
   );
+
+  //
+  // const [like, setLike] = useState(false);
+  // const color ='white';
+  //
+  // return (
+  //   <div className="vertical-navigation-container">
+  //     {like ? (
+  //       <RiHeart3Fill
+  //         fill={color}
+  //         onClick={() => {
+  //           setLike(false);
+  //         }}
+  //         className="icon"
+  //       />
+  //     ) : (
+  //       <RiHeart3Line
+  //         fill={color}
+  //         onClick={() => {
+  //           setLike(true);
+  //         }}
+  //         className="icon"
+  //       />
+  //     )}
+  //     <RiQuestionAnswerLine fill={color} className="icon" />
+  //     <RiShareForwardFill fill={color} className="icon" />
+  //     <RiListUnordered fill={color} className="icon" />
+  //   </div>
+  // );
 };
 
-export { BottomNavigation, TopNavigation, VerticalNavigation };
+
+
+function TopBar(
+  {
+    beforeButton,
+    beforeButtonOnClickEvent,
+    leftText,
+    leftTextType,
+    centerText,
+    centerTextType,
+    alarm,
+    alarmOnClickEvent,
+    checkButton,
+    checkButtonOnClickEvent,
+    dropdown,
+    dropdownList,
+  }
+  :ITopBar
+){
+
+  const [dropDown, setDropDown] = useState<boolean>(false);
+
+  return(
+    <div className={"top-bar-container"}>
+    <div className={"top-bar-inner-left-container"}>
+    {/* 뒤로가기 / 왼쪽 텍스트 */}
+      {/*  뒤로가기*/}
+        {
+          beforeButton
+          ?      <div onClick={beforeButtonOnClickEvent} className={"before-button-container"}>
+            <RiArrowLeftLine className={"icon"} />
+          </div>
+            :null
+        }
+
+      <div className={"left-text-container"}>
+      {/* 왼쪽 텍스트 */}
+        <h1 className={(leftTextType !== null && leftTextType === "user")?"top-bar-inner-text-type-user":"top-bar-inner-text-type-page"}>{leftText}</h1>
+      </div>
+    </div>
+
+      {centerText === ""
+        ? null
+        : <div className={`top-bar-inner-center-container`}>
+      {/* 가운데 텍스트 */}
+        <h1 className={(centerTextType !== null && centerTextType === "user")?"top-bar-inner-text-type-user":"top-bar-inner-text-type-page"}>{centerText}</h1>
+        </div>
+      }
+
+      <div className={"top-bar-inner-right-container"}>
+      {/* 알람 아이콘 / 체크 아이콘 / 드롭다운 메뉴 */}
+        {/* 알람 */}
+        {
+          alarm
+          ?        <div onClick={alarmOnClickEvent} className={"right-icon-container"} >
+              <>
+                <RiNotification2Line className={"icon"}/>
+              </>
+            </div>
+            : null
+        }
+
+
+      {/* 드롭다운 */}
+        {
+          dropdown
+            ? <div onClick={()=>{setDropDown((prev)=>!prev)}} className={"right-icon-container"} >
+                <RiMore2Fill className={"icon"} />
+                  {
+                    (dropdownList !== undefined && dropDown )?
+                      <div className={"dropdown-container"}>
+                        {
+                          dropdownList.map((item, i) => {
+                            return <div className={"dropdown-list-item"}>{item}</div>
+                          })
+                        }
+                      </div>
+                      : null
+                  }
+            </div>
+            : null
+        }
+
+        {/* 체크 */}
+        {
+          checkButton
+            ?  <div onClick={checkButtonOnClickEvent} className={"right-icon-container"} >
+              <RiCheckFill className={"icon"} />
+            </div>
+            : null
+        }
+
+      </div>
+    </div>
+)
+}
+
+export { TopBar,BottomNavigation, TopNavigation, VerticalNavigation };
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import '../styles/pages/MyPage.css';
+import { TopBar } from "../components/common/Navigation";
 
 function MyPage() {
   return (
-    <div className="container">
-      MyPage
+    <div className="my-page-container">
+    <TopBar
+      checkButtonOnClickEvent={()=>{console.log("확인")}}
+      beforeButtonOnClickEvent={()=>{console.log("뒤로가기")}}
+      alarmOnClickEvent={()=>{alert("alarm")}}
+      leftTextType={"user"}
+      centerText={"수진"}
+      centerTextType={"user"}
+      alarm={true}
+      beforeButton={true}
+      checkButton={true}
+      leftText={"수진"}
+      dropdown={true}
+      dropdownList={["프로필 편집", "개인정보 설정", "설정"]}
+    />
+
+
     </div>
   );
 }
