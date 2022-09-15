bef1527fe487483081b056ff4e97c7e17db18cad
su-lim <sujin.0xlim@gmail.com>
2022-09-15 20:11:57 +0900

    style: home

--- src/components/Modal/Vertical/BottomModalPage.tsx
+++ src/components/Modal/Vertical/BottomModalPage.tsx
@@ -52,9 +52,8 @@ export default function BottomModalPage({
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity " />
         </Transition.Child>
 
-        <div className="fixed z-[999] inset-0 overflow-y-auto">
-          <div className="flex items-center justify-center min-h-full p-4 text-center sm:p-0">
-
+        <div className="fixed z-[999] inset-0 overflow-y-auto ">
+          <div className="flex items-end min-h-full text-center sm:p-0">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
@@ -64,7 +63,6 @@ export default function BottomModalPage({
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-
               <Dialog.Panel
                 // className={`relative bg-white rounded-lg px-4 pt-2 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg w-full ${sizes[size]} sm:p-6 `}
                 className={'share-modal-container'}
@@ -72,7 +70,12 @@ export default function BottomModalPage({
                 <div>
                   <div className="mt-3 text-center sm:mt-5">
                     {/* header */}
-                    {close && <RiCloseFill className="w-6 h-6 absolute right-5 top-5" onClick={() => setOpen(false)}></RiCloseFill>}
+                    {close && (
+                      <RiCloseFill
+                        className="w-6 h-6 absolute right-5 top-5"
+                        onClick={() => setOpen(false)}
+                      ></RiCloseFill>
+                    )}
                     {/*{title && (*/}
                     {/*  <Dialog.Title as="h3" className="pt-1 text-lg leading-6 font-medium text-gray-900">*/}
                     {/*    {title}*/}
--- src/components/Modal/Vertical/Share.tsx
+++ src/components/Modal/Vertical/Share.tsx
@@ -1,17 +1,22 @@
-import React, { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction } from 'react';
 import '../../../styles/components/Modal/Vertical/Share.css';
-import { RiCloseFill } from "react-icons/ri";
+import { RiCloseFill } from 'react-icons/ri';
 
-interface IShare{
-  setViewShare : Dispatch<SetStateAction<boolean>>;
+interface IShare {
+  setViewShare: Dispatch<SetStateAction<boolean>>;
 }
 
-function Share({setViewShare}:IShare){
-  return(
+function Share({ setViewShare }: IShare) {
+  return (
     <div className={'share-modal-container'}>
       <div className={'share-modal-top-nav'}>
         <span className={'share-modal-top-nav-text'}>공유</span>
-        <div className={'share-modal-close-icon-container'} onClick={()=>{setViewShare(false)}}>
+        <div
+          className={'share-modal-close-icon-container'}
+          onClick={() => {
+            setViewShare(false);
+          }}
+        >
           <RiCloseFill className={'close-icon'} size={30} />
         </div>
       </div>
