6f6566e56725c879833aa7180fbd7e1f626ca723
kitez <duswl339@ajou.ac.kr>
2022-09-16 12:58:42 +0900

    feat: add local login & id duplicate api

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -10,6 +10,7 @@ function SignUpPage(){
   const [ pw, setPW ] = useState('');
   const [ pw2, setPW2 ] = useState('');
   const [ buttonActive , setButtonActive ] = useState(false);
+  const [ duplicate, setDuplicate ] = useState(false);
 
   useEffect(()=>{
     if( id.length > 4  && pw.length > 8 && pw2.length > 8 && pw === pw2){
@@ -20,23 +21,24 @@ function SignUpPage(){
     }
   },[id,pw,pw2])
 
-  const onClickSignUp = () => {
-    let flag = false;
-
+  const onClickDuplicate = () => {
+    console.log("onClickDuplicate");
+    // TODO:ID 중복확인
     axios.get(`${SERVER_API}/users/${id}/duplicate`)
       .then((res)=>{
         if(res.status === 200){
-          flag = true;
+          setDuplicate(true);
         }
         console.log("`${SERVER_API}/users/${id}/duplicate` : ",res);
       })
       .catch((err)=>{
-        console.log("`${SERVER_API}/users/${id}/duplicate` : ",err);
-        flag = false;
+          alert('중복된 ID입니다.');
       });
+  }
 
 
-    if(flag){
+  const onClickSignUp = () => {
+    // TODO: SIGNUP API
       axios.post(`${SERVER_API}/auth/signup`,{
         email : id,
         password : pw,
@@ -54,9 +56,6 @@ function SignUpPage(){
           console.log("${SERVER_API}/auth/signup",err);
           alert("로그인 실패");
         })
-    }else{
-      alert("이미 존재하는 ID입니다.");
-    }
   }
 
   return(
@@ -67,6 +66,7 @@ function SignUpPage(){
             <Input onChangeEventHandler={(e)=>{setID(e.currentTarget.value)}} label={"ID"} type={"text"} id={"id"} disabled={false} placeholder={"ID를 입력해주세요."} />
             <Input onChangeEventHandler={(e)=>{setPW(e.currentTarget.value)}} label={"PW"} type={"password"} id={"pw"} disabled={false} placeholder={"PW를 입력해주세요."} />
             <Input onChangeEventHandler={(e)=>{setPW2(e.currentTarget.value)}} label={"PW Check"} type={"password"} id={"pw2"} disabled={false} placeholder={"PW를 입력해주세요."} />
+            <button onClick={duplicate ?  ()=>{console.log()}: onClickDuplicate } className={duplicate ?"active-submit-button" :  "submit-button"}>ID 중복확인</button>
             <button onClick={buttonActive ? onClickSignUp : ()=>{console.log()}} className={buttonActive ?"active-submit-button" :  "submit-button"}>완료</button>
           </div>
       </div>
