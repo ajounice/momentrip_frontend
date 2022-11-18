895d697a5d906f8b305386b211d2271b1f669656
kitez <duswl339@ajou.ac.kr>
2022-11-18 10:17:09 +0900

    feat: API response interface file 추가

---
+++ src/Interface/ApiInterface.ts
@@ -0,0 +1,89 @@
+export type WishType = "TOUR" | "FORM";
+
+export interface Login{
+  accessToken : string;
+}
+
+export interface SignUp{
+  email : string;
+  password : string;
+  nickname : string;
+  name : string;
+  image : string;
+  id : number;
+  type : boolean;
+}
+
+export interface MyInfo{
+  id : number;
+  email : string;
+  nickname : string;
+  password : string;
+  name : string;
+  intro : string;
+  type : boolean;
+  image : string;
+}
+
+export interface OtherInfo{
+  nickname : string;
+  name : string;
+  intro : string;
+  type : boolean;
+  image : string;
+}
+
+export interface TourInfo{
+  id : number;
+  name : string;
+  place : string;
+  image : string;
+  price : number;
+  viewCount : number;
+}
+
+export interface Tag{
+  id : number;
+  name : string;
+}
+
+export interface Form{
+  id : number;
+  content : string;
+  title : string;
+  thumbnail : string;
+  video : string;
+  viewCount : number;
+}
+
+export interface SpecificForm{
+  id : number;
+  content : string;
+  title : string;
+  thumbnail : string;
+  video : string;
+  viewCount : number;
+  tags : Tag[];
+  user : MyInfo;
+  tourInfo : TourInfo;
+}
+
+export interface FormComment{
+  id : number;
+  content : string;
+  form : Form;
+  user : MyInfo;
+}
+
+export interface Wish{
+  id : number;
+  type: WishType;
+  targetId : number;
+}
+
+export interface WishFolder{
+  id : number;
+  name : string;
+  wishlists : Wish[];
+}
+
