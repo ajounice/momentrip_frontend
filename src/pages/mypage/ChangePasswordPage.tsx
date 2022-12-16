e64a631dc612945e1424fa00161d08dcb39b91a5
kitez <duswl339@ajou.ac.kr>
2022-12-16 19:57:59 +0900

    feat: 비밀번호 변경 API 연동 및 결과에 따른 모달 추가
    + API 요청 후 나와야 하는 모달과 관련된 state (modalView)
    
    + Input에 입력한 값 changePW와 changePW2가 일치하는지 일치하지 않는지 보여주기 위한 state (view)
    
    + 비밀번호 변경 성공하면 /mypage/setting으로 이동
    
    + 비밀번호 변경 실패하면 모달 안보이게

--- src/pages/mypage/ChangePasswordPage.tsx
+++ src/pages/mypage/ChangePasswordPage.tsx
@@ -1,41 +1,170 @@
 import { TopBar } from "../../components/common/Navigation";
 import Input from "../../components/common/Input";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import '../../styles/pages/mypage/Setting.css';
+import axios from "axios";
+import AlertModal from "../../components/common/AlertModal";
+import CustomModal from "../../components/common/CustomModal";
+import Button from "../../components/Button/Button";
+import BasicModal from "../../components/common/BasicModal";
+import { useNavigate } from "react-router-dom";
 
 function ChangePasswordPage() {
+  const [ password, setPassword ] = useState({
+    'currentPW': '',
+    'changePW': '',
+    'changePW2': '',
+  });
+
+  const navigation = useNavigate();
+
+  const [view, setView] = useState(false);
+  const [modalView, setModalView] = useState({
+    view: false,
+    result: ''
+  });
+
+  const onPressEnter = (e: any) => {
+     if(e.code === 'Enter'|| e.type === 'click') {
+        if(view && password.changePW === password.changePW2) {
+          axios
+            .patch(`${process.env.REACT_APP_API_URL}/users/my/edit/password`,{
+              password: password.changePW,
+              passwordConfirmation: password.currentPW
+            }, {
+              headers: {
+                Authorization: `Bearer ${window.localStorage.getItem('Token')}`,
+              },
+            })
+            .then((res) => {
+              if(res.status === 200) {
+                setModalView({view: true, result: 'success'});
+              }
+            })
+            .catch((err) => {
+              setModalView({view: true, result: 'fail'});
+            });
+        }
+      }
+  }
+
+  const onChangeInput = (e: any) => {
+    const temp = JSON.parse(JSON.stringify(password));
+    temp[e.target.id] = e.target.value;
+    setPassword(temp);
+  }
+
+  useEffect(()=> {
+    // console.log(password);
+    // 1. 정규식 체크
+    // 2. change password === change password 2 check
+    if(password.changePW.length >= 8 && password.changePW2.length >= 8) {
+      setView(true);
+    }
+    else {
+      setView(false);
+    }
+
+  },[password]);
+
   return(
     <div className={'change-password-container'}>
-      <TopBar beforeButton={true} centerText={'비밀번호 설정'} centerTextType={'page'} />
-      <div className={'password-change-inner-container'}>
-        <div className={'password-change-input-container'}>
-          <Input
-            label={'현재 비밀번호'}
-            type={'password'}
-            id={'currentPW'}
-            disabled={false}
-            placeholder={'현재 비밀번호를 입력하세요'}
-          />
-          <Input
-            label={'변경할 비밀번호'}
-            type={'password'}
-            id={'changePW'}
-            disabled={false}
-            placeholder={'변경할 비밀번호를 입력하세요.'}
-          />
-          <Input
-            label={'변경할 비밀번호'}
-            type={'password'}
-            id={'changePW2'}
-            disabled={false}
-            placeholder={'변경할 비밀번호를 입력하세요.'}
-          />
+      {
+        modalView.view
+          ?  <>
+            {
+              modalView.result === 'success'
+              ?
+              <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+                <BasicModal title="비밀번호 변경에 성공하였습니다." open={true} setOpen={() => {navigation('/mypage/setting')}}>
+                  <>
+                    <div className="m-8"> </div>
+                    <div className="flex gap-2">
 
-          <button className={'password-change-button'} type={'submit'}>
-            확인
-          </button>
-        </div>
-      </div>
+                    </div>
+                  </>
+                </BasicModal>
+              </div>
+                :
+                <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+                  <BasicModal title="비밀번호 변경에 실패하였습니다." open={true} setOpen={() => {setModalView({view: false, result: ''})}}>
+                    <>
+                      <div className="m-8"> </div>
+                      <div className="flex gap-2">
+                      </div>
+                    </>
+                  </BasicModal>
+                </div>
+            }
+          </>
+          : <>
+            <TopBar beforeButton={true} centerText={'비밀번호 설정'} centerTextType={'page'} />
+            <div className={'password-change-inner-container'}>
+              <div className={'password-change-input-container'}>
+                <div>
+                  <Input
+                    onPress={onPressEnter}
+                    label={'현재 비밀번호'}
+                    type={'password'}
+                    id={'currentPW'}
+                    disabled={false}
+                    onChangeEventHandler={onChangeInput}
+                    placeholder={'현재 비밀번호를 입력하세요'}
+                  />
+                </div>
+
+                <div className={'input-div'}>
+                  <Input
+                    onPress={onPressEnter}
+                    label={'변경할 비밀번호'}
+                    type={'password'}
+                    id={'changePW'}
+                    disabled={false}
+                    onChangeEventHandler={onChangeInput}
+                    placeholder={'변경할 비밀번호를 입력하세요.'}
+                  />
+                  {
+                    password.changePW.length < 8 && password.changePW.length !== 0 ?
+                      <span>8자 이상 입력하세요.</span>
+                      : <></>
+                  }
+                </div>
+
+
+                <div className={'input-div'}>
+                  <Input
+                    onPress={onPressEnter}
+                    label={'변경할 비밀번호'}
+                    type={'password'}
+                    id={'changePW2'}
+                    disabled={false}
+                    onChangeEventHandler={onChangeInput}
+                    placeholder={'변경할 비밀번호를 입력하세요.'}
+                  />
+                  {
+                    password.changePW2.length < 8 && password.changePW2.length !== 0 ?
+                      <span>8자 이상 입력하세요.</span>
+                      : <></>
+                  }
+                </div>
+                {
+                  view
+                    ?<div>
+                      {
+                        password.changePW === password.changePW2
+                          ? <span className={'pw-pw2-correct'}>비밀번호가 일치합니다.</span>
+                          :<span className={'pw-pw2-incorrect'}>비밀번호가 일치하지 않습니다.</span>
+                      }
+                    </div>
+                    : <></>
+                }
+
+                <button onClick={onPressEnter} className={'password-change-button'} type={'submit'}>
+                  확인
+                </button>
+              </div>
+            </div></>
+      }
     </div>
   )
 }
